10a1ef7701c0aba459da1bca35d8cfd15b6d3e6c
Gustavo Ramos <gustavoramosrock@gmail.com>
2022-04-10 23:03:41 -0300

    configuring React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,12 +1,24 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import Header from "../../components/Header";
 import { Sidebar } from "../../components/SideBar";
 import { Pagination } from "../../components/Pagination";
 import Link from "next/link";
 import { useEffect } from "react";
+import { useQuery } from "react-query";
+
+
 
 export default function UserList(){
+
+	const {data, isLoading, error} = useQuery('users', async ()=>{
+		const response = await fetch('http://localhost:3000/api/users')
+		const data = await response.json()
+		
+		return data
+	})
+
+
 	const isWideVersion = useBreakpointValue({
 		base: false,
 		lg: true,
@@ -42,57 +54,70 @@ export default function UserList(){
 
 					</Flex>
 
+				{ isLoading ? (
+					<Flex justify='center'>
+						<Spinner />
+					</Flex>
+				) : error ? (
+					<Flex justify="center">
+						<Text>Falha ao obter dados dos usuários</Text>
+					</Flex>
+				) : (
+					<>
 					<Table colorScheme="whiteAlpha">
-						<Thead>
-							<Tr>
-								<Th px={["4","4","6"]} color="gray.300" width="8">
-									<Checkbox colorScheme="pink"/>
-								</Th>
-								<Th>Usuário</Th>
-								{isWideVersion && <Th>Data de cadastro</Th>}
-							</Tr>
-						</Thead>
-						<Tbody>
-							<Tr>
-								<Td px={["4","4","6"]}>
-									<Checkbox colorScheme="pink"/>
-								</Td>
-								<Td>
-									<Box>
-										<Text fontWeight="bold">Gustavo Ramos</Text>
-										<Text fontSize="sm" color="gray.300">gustavo.ramos.silva.santos@gmai.com</Text>
-									</Box>
-								</Td>
-								{ isWideVersion && <Td>22 de outubro, 2021</Td>}
-							</Tr>
-							<Tr>
-								<Td px={["4","4","6"]}>
-									<Checkbox colorScheme="pink"/>
-								</Td>
-								<Td>
-									<Box>
-										<Text fontWeight="bold">Gustavo Ramos</Text>
-										<Text fontSize="sm" color="gray.300">gustavo.ramos.silva.santos@gmai.com</Text>
-									</Box>
-								</Td>
-								{ isWideVersion && <Td>22 de outubro, 2021</Td>}
-							</Tr>
-							<Tr>
-								<Td px={["4","4","6"]}>
-									<Checkbox colorScheme="pink"/>
-								</Td>
-								<Td>
-									<Box>
-										<Text fontWeight="bold">Gustavo Ramos</Text>
-										<Text fontSize="sm" color="gray.300">gustavo.ramos.silva.santos@gmai.com</Text>
-									</Box>
-								</Td>
-								{ isWideVersion && <Td>22 de outubro, 2021</Td>}
-							</Tr>
-						</Tbody>
+					<Thead>
+						<Tr>
+							<Th px={["4","4","6"]} color="gray.300" width="8">
+								<Checkbox colorScheme="pink"/>
+							</Th>
+							<Th>Usuário</Th>
+							{isWideVersion && <Th>Data de cadastro</Th>}
+						</Tr>
+					</Thead>
+					<Tbody>
+						<Tr>
+							<Td px={["4","4","6"]}>
+								<Checkbox colorScheme="pink"/>
+							</Td>
+							<Td>
+								<Box>
+									<Text fontWeight="bold">Gustavo Ramos</Text>
+									<Text fontSize="sm" color="gray.300">gustavo.ramos.silva.santos@gmai.com</Text>
+								</Box>
+							</Td>
+							{ isWideVersion && <Td>22 de outubro, 2021</Td>}
+						</Tr>
+						<Tr>
+							<Td px={["4","4","6"]}>
+								<Checkbox colorScheme="pink"/>
+							</Td>
+							<Td>
+								<Box>
+									<Text fontWeight="bold">Gustavo Ramos</Text>
+									<Text fontSize="sm" color="gray.300">gustavo.ramos.silva.santos@gmai.com</Text>
+								</Box>
+							</Td>
+							{ isWideVersion && <Td>22 de outubro, 2021</Td>}
+						</Tr>
+						<Tr>
+							<Td px={["4","4","6"]}>
+								<Checkbox colorScheme="pink"/>
+							</Td>
+							<Td>
+								<Box>
+									<Text fontWeight="bold">Gustavo Ramos</Text>
+									<Text fontSize="sm" color="gray.300">gustavo.ramos.silva.santos@gmai.com</Text>
+								</Box>
+							</Td>
+							{ isWideVersion && <Td>22 de outubro, 2021</Td>}
+						</Tr>
+					</Tbody>
 					</Table>
 
 					<Pagination />
+					</>
+				)}
+
 				</Box>
 			</Flex>
 		</Box>
