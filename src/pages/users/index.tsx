310dd57ed6f9df3f5ceb516201df6cffd558f7aa
Gustavo Ramos <gustavoramosrock@gmail.com>
2022-05-15 20:15:25 -0300

    data prefetch

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,13 +1,14 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue, Spinner, Link } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import Header from "../../components/Header";
 import { Sidebar } from "../../components/SideBar";
 import { Pagination } from "../../components/Pagination";
-import Link from "next/link";
+import NextLink from "next/link";
 import { useEffect, useState } from "react";
-import { useQuery } from "react-query";
+import { QueryClient, useQuery } from "react-query";
 import { api } from "../../services/api";
 import {useUsers} from '../../services/hooks/useUsers'
+import { queryClient } from "../../services/queryClient";
 
 
 export default function UserList(){
@@ -21,6 +22,17 @@ export default function UserList(){
 		lg: true,
 	})
 	
+	async function handlePrefetchUser(userId: string){
+		await queryClient.prefetchQuery(['user', userId], async()=>{
+			const response = await api.get(`users/${userId}`)
+
+			return response.data;
+		},{
+			staleTime: 1000 * 60 * 10
+		})
+
+		
+	}
 	return(
 		<Box>
 			<Header/>
@@ -37,7 +49,7 @@ export default function UserList(){
 
 							
 
-						<Link href="users/create" passHref>
+						<NextLink href="users/create" passHref>
 							<Button
 							as="a"
 							size="sm"
@@ -47,7 +59,7 @@ export default function UserList(){
 							>
 								Criar novo
 							</Button>
-						</Link>
+						</NextLink>
 
 					</Flex>
 
@@ -80,7 +92,11 @@ export default function UserList(){
 									</Td>
 									<Td>
 										<Box>
-											<Text fontWeight="bold">{user.name}</Text>
+											<Link color="purple.400" onMouseEnter={()=>{
+												handlePrefetchUser(user.id)
+											}}>
+												<Text fontWeight="bold">{user.name}</Text>
+											</Link>
 											<Text fontSize="sm" color="gray.300">{user.email}</Text>
 										</Box>
 									</Td>
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -45,7 +45,7 @@ export async function getUsers(page: number): Promise<GetUsersResponse>{
 export function useUsers(page: number){
 	return(
 		useQuery(['users', page], ()=> getUsers(page),{
-			staleTime: 1000 * 5, // 5sec.
+			staleTime: 1000 * 60 * 10, // 10min.
 		}
 	))
 }
